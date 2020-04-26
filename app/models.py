dd11373582fa2adacaaa20d8a6e0eb303fd9b218
RitikUpmanyu <ritikup008@gmail.com>
2020-04-26 15:26:51 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -61,10 +61,10 @@ db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
 def load_user(id):
     return User.query.get(int(id))
 
-class User(UserMixin, db.Model):
+class User(UserMixin, SearchableMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    #__searchable__ = ['username']
+    __searchable__ = ['username']
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
